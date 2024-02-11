62cc9fdeabfdcd66785ca670157d90f5bd1f2f8f
kumarpritam1468 <manoharipritam@gmail.com>
2024-02-11 21:35:13 +0530

    initial commit

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req,res) => {
+router.route('/').get((req,res) => {
     res.status(200).send("Hello there!");
 });
 
--- server/server.js
+++ server/server.js
@@ -5,10 +5,6 @@ const router = require("./router/auth-router");
 
 app.use("api/auth", router);
 
-app.get('/', (req,res) => {
-    res.status(200).send("Hello there!");
-})
-
 app.listen(PORT, ()=>{
     console.log(`Server running on port : ${PORT}`);
 })
