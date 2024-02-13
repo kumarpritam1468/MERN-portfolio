6e2b26795f7b872b7a9e164fb0e847a9940c9f54
kumarpritam1468 <manoharipritam@gmail.com>
2024-02-13 17:49:35 +0530

    add data storing

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,4 +1,6 @@
-const home = async (req,res) =>{
+const User = require("../models/userModel");
+
+const home = async (req, res) => {
     try {
         res.status(200).send("Hello there!");
     } catch (error) {
@@ -6,13 +8,23 @@ const home = async (req,res) =>{
     }
 }
 
-const register = async (req,res) =>{
+const register = async (req, res) => {
     try {
-        console.log(req.body);
-        res.status(200).json({message:req.body});
+        const { name, phone, email, password } = req.body;
+
+        const userExist = await User.findOne({ email });
+
+        if (userExist) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const userCreated = await User.create({ name, phone, email, password });
+
+        res.status(200).json({ userCreated });
+
     } catch (error) {
-        res.status(500).json({message:error});
+        res.status(500).json({ message: error });
     }
 }
 
-module.exports = {home, register};
+module.exports = { home, register };
---
+++ server/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+});
+
+const User = new mongoose.model("User", userSchema);
+
+module.exports = User;
