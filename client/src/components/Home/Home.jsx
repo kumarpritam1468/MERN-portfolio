de2a2953d80b25521e73dde0bf9f4c8744e73646
kumarpritam1468 <manoharipritam@gmail.com>
2024-02-19 17:12:38 +0530

    home page done

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -23,6 +23,30 @@ const Home = () => {
           <img src="./images/home.png" alt="" />
         </div>
       </div>
+
+
+      <div className="hero heroEnd">
+        <div className="left">
+          <h1>Get Started with <span className="colorText">Pritam</span></h1>
+          <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate soluta consequatur sint corporis eos dolorem fugit aut ipsa quam neque.</h3>
+          <div className="buttons">
+            <Link className="btn1" to="/contact" >
+              Contact Now
+            </Link>
+            <Link className="btn2" to="/about" >
+              Know More
+            </Link>
+          </div>
+        </div>
+
+        <div className="right">
+          <img src="./images/design.png" alt="" />
+        </div>
+      </div>
+
+      <div className="footer">
+        &copy; Pritam Kumar Manohari
+      </div>
     </>
   )
 }
